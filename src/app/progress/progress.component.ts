4c3ce4fcfee5aead843f24bcfa569587364adb7e
Martin Picard <martin.picard@emc.com>
2018-01-25 08:53:36 +0000

    Add unsubscribe to progress

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from "rxjs/Rx";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from "rxjs/Rx";
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.styl']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent implements OnInit, OnDestroy {
 
   private progress = 0;
 
+  sub: Subscription;
+
   ngOnInit() {
-    Observable.timer(0, 1000).subscribe(() => {
-      this.progress += 1;
-    });
+    this.sub = Observable.timer(0, 1000)
+      .filter(() => this.progress < 100)
+      .subscribe(() => {
+        this.progress += 1;
+      });
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
